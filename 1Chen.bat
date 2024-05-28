598c64ad23d453c3828dbde3457ada7e5f50cde9
Chen <Ciallo.sjtu.chen@gmail.com>
2024-05-28 19:10:48 +0800

    1

--- 1Chen.bat
+++ 1Chen.bat
@@ -1,5 +1,14 @@
 @echo off
 cd d:/SPEIT/Chen
+
+:: 提示用户输入提交信息
+set /p commitMessage="Enter commit message: "
+
+:: 添加更改到暂存区
 git add .
-git commit -a
+
+:: 提交更改并使用用户输入的提交信息
+git commit -m "%commitMessage%"
+
+:: 推送更改到远程仓库
 git push
